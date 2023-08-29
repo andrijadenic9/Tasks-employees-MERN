8f6eb36e14affe6dd12fed0ea2a63a08a3539b2f
andrijadenic9 <andrijadenic9@gmail.com>
2023-08-29 13:51:16 +0200

    readme

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -7,7 +7,7 @@ function Navbar({ isEmployeePage, isTaskPage, setIsEmployeeAddModal, setIsTaskAd
         <>
             <nav className="navbar navbar-expand-lg bg-body-tertiary">
                 <div className="container">
-                    <a className="navbar-brand">Andrija Denic</a>
+                    <a className="navbar-brand">SpectrumTech Solutions</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
